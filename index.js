f17dba619385366ad84b9503031a3c11de1067b3
Daniel Mason <danmasson0@gmail.com>
2021-08-07 01:32:32 -0400

    refactored code

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ function removeLoadingSpinner() {
   loader.hidden = true;
 }
 
-// Show new Quote
 function newQuote() {
   showLoadingSpinner();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  // Check if Author field is blacnk and replace with unknown
+  // Check if Author field is black and replace with unknown
   if (!quote.author) {
     authorText.textContent = "Phuong Nguyen";
   } else {
@@ -36,7 +35,6 @@ function newQuote() {
   removeLoadingSpinner();
 }
 
-// Get Quotes from Api
 async function getQuotes() {
   showLoadingSpinner();
   const apiURL = "https://type.fit/api/quotes";
@@ -49,13 +47,11 @@ async function getQuotes() {
   }
 }
 
-// Tweet a Quote
 function tweetQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
   window.open(twitterUrl, "_blank");
 }
 
-// Event listener
 newQuoteBtn.addEventListener("click", newQuote);
 twitterBtn.addEventListener("click", tweetQuote);
 
