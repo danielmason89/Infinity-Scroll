81007a11958c8570699e939c027a0542fc70535a
Daniel Mason <danmasson0@gmail.com>
2022-06-12 11:02:49 -0400

    Implement unsplash API, to fetch data and used some javascript to see that the request was being completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("testing");
+// Unsplash API
+
+const count = 10;
+const apiKey = "ROoxCgg5evi_T5KjvFcCjh824Ixl0TxKsNqORBTFVDQ";
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Get Photos from Unsplash API
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    // console.log(error);
+  }
+}
+
+// On Load
+getPhotos();
